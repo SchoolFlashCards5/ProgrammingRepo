a082dd0efbd059ccd9b844420ae3ef6d3d07bee9
SchoolFlashCards3 <cheesyboiw3@gmail.com>
2026-01-29 19:09:55 -0600

    Enhance gambling game with new commands and logic
    Refactor gambling commands and add reset and cheat code features.

--- gamble.py
+++ gamble.py
@@ -1,4 +1,3 @@
-
 import random
 import time
 
@@ -10,12 +9,7 @@ print("Use the ? command for info and commands")
 while True:
  print("")
  cmd = input("Enter command here: ")
- if cmd == "printe":
-  printe = input("Enter text to essentially just echo: ")
-  print("")
-  print(printe)
-  print("")
- elif cmd == "?":
+ if cmd == "?":
   print("About:")
   print("ver 1.0")
   print("Made by zach, currently only colorless roulette, and number only lol")
@@ -25,13 +19,16 @@ while True:
  elif cmd == "bet":
   bet = int(next(i for i in iter(lambda: input("Enter number: "), '') if i.isdigit()))
   if a >= bet:
-   randomroll = random.randint(1, 2)
-   if randomroll == 1:
-    print("You got it!")
-    a += bet
+   if bet > 0:
+    randomroll = random.randint(1, 2)
+    if randomroll == 1:
+     print("You got it!")
+     a += bet
+    else:
+     print("You need to stop gambling.")
+     a -= bet
    else:
-    print("You need to stop gambling.")
-    a -= bet
+     print("you cant bet 0 twin")
   elif a == 0:
    print("Sorry, you dont have ANY chips. Use the quit command to exit the game, then restart the program to reset back to 100.") 
   else:
@@ -39,9 +36,16 @@ while True:
    print("")
  elif cmd == "balance":
   print("")
-  print(a)
+  print(f"${a}")
  elif cmd == "quit":
+  print("exiting")
   break
+ elif cmd == "reset":
+  a = 100
+  print("Reset succesful")
+ elif cmd == "moneymoney":
+  a += 1000
+  print("Cheat code activated, +1000 money!!!!!!!")
  else:
   print("")
   print("man that aint no command try again")

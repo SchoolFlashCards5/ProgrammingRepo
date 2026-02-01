fa3ad385f2347431e4a911f4da6e82aa02523ef6
SchoolFlashCards3 <cheesyboiw3@gmail.com>
2026-01-31 23:36:06 -0600

    Add files via upload

---
+++ coinflip/script.js
@@ -0,0 +1,30 @@
+let bal = 100;
+let winorlose = "";
+document.getElementById("balance").textContent = bal; //prints out balance before anything so user knows what they start with
+
+function resetbal() {
+  bal = 100 //resets balance to 0
+  winorlose = "balance reset"; //resets win or lose text to balance reset to assure user it was reset to 100
+  document.getElementById("balance").textContent = bal; //printsout balance so the user has visual proof of reset
+  console.log("balance reset to 100")
+}
+function placeBet() {
+  let betAmount = Number(document.getElementById("betInput").value);
+  if (betAmount <= 0 || betAmount > bal) {
+    alert("Invalid bet; you cant bet more than you have or 0 or a negative OR more than you have")
+    return;
+  }
+
+  if (Math.random() < 0.5) {
+    bal += betAmount; //win does plus bet amm
+    winorlose = "you won"; //sets win or lose display text to win
+    console.log("Balance is now " + bal)
+  } else {
+    bal -= betAmount; //lose does minus bet amm
+    winorlose = "sorry you lost :("; //sets win or lose display text to win
+    console.log("Balance is now " + bal)
+  }
+
+  document.getElementById("balance").textContent = bal;
+  document.getElementById("winorlose").textContent = winorlose;
+}
